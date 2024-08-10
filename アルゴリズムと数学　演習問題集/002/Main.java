6a0cc2051719948303b0569edae54dda82ba5e84
MakotoTaguchi <taguchimakoto@icloud.com>
2024-08-10 20:44:08 +0900

    002

---
+++ アルゴリズムと数学　演習問題集/002/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // スペース区切りの整数の入力
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        // 出力
+        System.out.println(a + b + c);
+    }
+}
